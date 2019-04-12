a7850bfe22f99c58e6f86292ead14863f016c62c
Jared Harbison <jared.harbison@gmail.com>
2019-04-12 18:46:14 -0400

    added user microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ include SessionsHelper
 def index
 end
 
+def logged_in_user
+  unless logged_in?
+    store_location
+    flash[:danger] = "please log in"
+    redirect_to login_url
+  end
+end
+
 end
