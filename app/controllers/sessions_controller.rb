ecb9e1ded8864f3f1cd1e4d6901d65c69e62bf23
Jared Harbison <jared.harbison@gmail.com>
2019-04-19 23:15:47 -0400

    pulled omni auth method to explore different accounts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,32 +3,33 @@ class SessionsController < ApplicationController
   def new
   end
 
+#  def create
+#    @user = User.find_or_create_by(email: auth['info']['email']) do |u|
+#      u.username = auth['info']['name']
+#      u.uid = auth['uid']
+#      u.image = auth['info']['image']
+#      u.password = SecureRandom.hex(12)
+#    end
+#    session[:user_id] = @user.id
+#    redirect_to root_url
+#  end
+
   def create
-    if @user = User.find_or_create_by(email: auth['info']['email']) do |u|
-        u.username = auth['info']['name']
-        u.uid = auth['uid']
-        u.image = auth['info']['image']
-        u.password = SecureRandom.hex(12)
-      end
-      session[:user_id] = @user.id
-      redirect_to root_url
-    else
-      user = User.find_by(email: params[:session][:email])
-      if user && user.authenticate(params[:session][:password])
-        if user.activated?
-          log_in user
-          params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-          redirect_back_or user
-        else
-          message = "account not activated"
-          message += "check your email for the activation link"
-          flash[:warning] = message
-          redirect_to root_url
-        end
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
       else
-        flash.now[:danger] = 'invalid email/password combination'
-        render 'new'
+        message = "account not activated"
+        message += "check your email for the activation link"
+        flash[:warning] = message
+        redirect_to root_url
       end
+    else
+      flash.now[:danger] = 'invalid email/password combination'
+      render 'new'
     end
   end
 
